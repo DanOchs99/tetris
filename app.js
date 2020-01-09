5d0b5684da2e16a629c6556860f13df42fb978bc
Rawand Dargali <rdargali@yahoo.com>
2020-01-09 13:55:29 -0600

    merge before pull

--- app.js
+++ app.js
@@ -14,20 +14,21 @@ const path = require("path");
 //routers
 const leaderboardRouter = require("./routes/leaderboard");
 app.use("/leaderboard", leaderboardRouter);
+
 const playRouter = require("./routes/play");
 app.use("/play", playRouter);
 
 app.use(express.static("public"));
 
 // configure view engine
-app.engine('mustache', mustacheExpress())
-app.set('views', './views')
-app.set('view engine', 'mustache')
+app.engine("mustache", mustacheExpress());
+app.set("views", "./views");
+app.set("view engine", "mustache");
 
-app.get('/',(req,res) => {
-    res.render('index')
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
