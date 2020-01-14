cae70b4b370fc6013e93af43d2fd0814d0e56edd
Dan Ochs <dochs@ochsholdings.com>
2020-01-14 10:47:11 -0600

    remove console logging from leaderboard.js

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -8,17 +8,17 @@ router.get("/", (req, res) => {
   db.any(
     "SELECT username, high_score FROM users, scores WHERE users.user_id = scores.user_id ORDER BY high_score DESC LIMIT 10;"
   ).then(results => {
-    console.log(results);
+    //console.log(results);
     const newResults = results.map((r, index) => {
       return {
         ...r,
         rankId: ++index
       };
     });
-    console.log(newResults);
+    //console.log(newResults);
     db.one('SELECT high_score FROM scores WHERE user_id = $1;', [userId])
       .then(finalResult => {
-        console.log(finalResult)
+        //console.log(finalResult)
         res.render("leaderboard", {
           userScore: finalResult,
           scores: newResults
