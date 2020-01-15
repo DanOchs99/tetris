95d698c2507bdb4f740f72c4f8377a399d0b6c72
johnpham47 <johnpham47@gmail.com>
2020-01-15 13:24:36 -0600

    Added prototype username functinality in sockets

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -7,33 +7,35 @@ const db = require("../db")
 
 router.get("/", (req, res) => {
     let userId = req.session.userId
+    let loggedInUsername = req.session.username
+
     db.any("SELECT username, high_score, high_score_date FROM users, scores WHERE users.user_id = scores.user_id ORDER BY high_score DESC LIMIT 10;")
-    .then(results => {
-        console.log(results);
-        const newResults = results.map((r, index) => {
-            return {username: r.username, high_score: r.high_score, high_score_date: r.high_score_date, rankId: ++index};
-        });
-        console.log(newResults);
-        db.one('SELECT current_score, high_score FROM scores WHERE user_id = $1;', [userId])
-        .then(finalResult => {
-            //console.log(finalResult)
-            res.render("leaderboard", {userScore: finalResult, scores: newResults})
+        .then(results => {
+            // console.log(results);
+            const newResults = results.map((r, index) => {
+                return { username: r.username, high_score: r.high_score, high_score_date: r.high_score_date, rankId: ++index };
+            });
+            // console.log(newResults);
+            db.one('SELECT current_score, high_score FROM scores WHERE user_id = $1;', [userId])
+                .then(finalResult => {
+                    //console.log(finalResult)
+                    res.render("leaderboard", { userScore: finalResult, scores: newResults, username: req.session.username })
+                })
+                .catch((error) => {
+                    console.log(error);
+                    if (req.session) {
+                        req.session.destroy();
+                    }
+                    res.render("landing", { message: "An error occurred..." })
+                });
         })
         .catch((error) => {
             console.log(error);
             if (req.session) {
-              req.session.destroy();
+                req.session.destroy();
             }
-            res.render("landing", {message: "An error occurred..."})  
+            res.render("landing", { message: "An error occurred..." })
         });
-    })
-    .catch((error) => {
-        console.log(error);
-        if (req.session) {
-          req.session.destroy();
-        }
-        res.render("landing", {message: "An error occurred..."})  
-    });
 });
 
 // socket.io repeater
@@ -42,5 +44,5 @@ io.on('connection', function (socket) {
         io.emit('chat message', msg)
     });
 });
-  
+
 module.exports = router;
