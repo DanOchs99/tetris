45b2cff351a28d7ed9be1f1fc4f07ee4e39e0ff4
Dan Ochs <dochs@ochsholdings.com>
2020-01-15 10:43:47 -0600

    minor comment and formatting at socket.io function

--- app.js
+++ app.js
@@ -178,14 +178,13 @@ app.post("/login", (req, res) => {
     });
 });
 
-// socket.io listener
+// socket.io repeater
 io.on('connection', function (socket) {
-  socket.on('chat message', function (msg) {
-    io.emit('chat message', msg)
-  })
-
-})
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+});
 
 http.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
